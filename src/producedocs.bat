f31eba80306ad1ae202cc34263ee2b788e031d5c
Simon Bull <sbulluk@users.noreply.github.com>
2016-10-17 08:43:49 +0100

    Change test output format to nunit2
    VSTS does not appear to read NUnit3 test result files, so trying to change to NUnit2 to allow publishing of results.

--- src/producedocs.bat
+++ src/producedocs.bat
@@ -1,8 +1,8 @@
 SET cfg=%1
 IF [%cfg%]==[] SET cfg=Debug
 
-packages\NUnit.ConsoleRunner.3.4.1\tools\nunit3-console.exe SFA.DAS.Payments.AcceptanceTests\bin\%cfg%\SFA.DAS.Payments.AcceptanceTests.dll --result=SFA.DAS.Payments.AcceptanceTests\bin\%cfg%\TestResult.xml
+packages\NUnit.ConsoleRunner.3.4.1\tools\nunit3-console.exe SFA.DAS.Payments.AcceptanceTests\bin\%cfg%\SFA.DAS.Payments.AcceptanceTests.dll --result=SFA.DAS.Payments.AcceptanceTests\bin\%cfg%\TestResult.xml;format=nunit2
 
 rmdir ..\docs /Q /S
 mkdir ..\docs
-packages\Pickles.CommandLine.2.8.2\tools\pickles.exe --feature-directory ..\features --link-results-file SFA.DAS.Payments.AcceptanceTests\bin\%cfg%\TestResult.xml --test-results-format nunit3 --output-directory ..\docs
+packages\Pickles.CommandLine.2.8.2\tools\pickles.exe --feature-directory ..\features --link-results-file SFA.DAS.Payments.AcceptanceTests\bin\%cfg%\TestResult.xml --test-results-format nunit2 --output-directory ..\docs
