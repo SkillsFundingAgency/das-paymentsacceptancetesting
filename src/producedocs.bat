03d5c378addbcfa774c31a147939dd1354680e44
Simon Bull <sbulluk@users.noreply.github.com>
2016-10-18 10:57:18 +0100

    return exit code from producedocs
    Return nunit exitcode to try and bubble failure to VSTS

--- src/producedocs.bat
+++ src/producedocs.bat
@@ -3,6 +3,12 @@ IF [%cfg%]==[] SET cfg=Debug
 
 packages\NUnit.ConsoleRunner.3.4.1\tools\nunit3-console.exe SFA.DAS.Payments.AcceptanceTests\bin\%cfg%\SFA.DAS.Payments.AcceptanceTests.dll --result=SFA.DAS.Payments.AcceptanceTests\bin\%cfg%\TestResult.xml;format=nunit2
 
+SET testresult=%ERRORLEVEL%
+
 rmdir ..\docs /Q /S
 mkdir ..\docs
 packages\Pickles.CommandLine.2.8.2\tools\pickles.exe --feature-directory ..\features --link-results-file SFA.DAS.Payments.AcceptanceTests\bin\%cfg%\TestResult.xml --test-results-format nunit --output-directory ..\docs
+
+IF %testresult% NEQ 0 (
+	exit /b %testresult%
+)
