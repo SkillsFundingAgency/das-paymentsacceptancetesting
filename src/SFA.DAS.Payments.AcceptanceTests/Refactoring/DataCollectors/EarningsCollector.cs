f73538ff94496b0d2c5ab88dadfcb48a5929e7cf
Simon Bull <simonbu11@users.noreply.github.com>
2017-03-09 11:48:11 +0000

    Remove unused namespace imports

--- src/SFA.DAS.Payments.AcceptanceTests/Refactoring/DataCollectors/EarningsCollector.cs
+++ src/SFA.DAS.Payments.AcceptanceTests/Refactoring/DataCollectors/EarningsCollector.cs
@@ -2,8 +2,6 @@
 using System.Collections.Generic;
 using System.Data.SqlClient;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Dapper;
 using SFA.DAS.Payments.AcceptanceTests.Refactoring.Contexts;
 using SFA.DAS.Payments.AcceptanceTests.Refactoring.DataCollectors.Entities;
--- src/SFA.DAS.Payments.AcceptanceTests/Refactoring/DataCollectors/PaymentsDataCollector.cs
+++ src/SFA.DAS.Payments.AcceptanceTests/Refactoring/DataCollectors/PaymentsDataCollector.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 using SFA.DAS.Payments.AcceptanceTests.Refactoring.Contexts;
 using SFA.DAS.Payments.AcceptanceTests.Refactoring.ResultsDataModels;
 
