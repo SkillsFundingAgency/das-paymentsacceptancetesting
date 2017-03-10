f94957192694449f192f76abe7816e6da33eea6d
Simon Bull <simonbu11@users.noreply.github.com>
2017-03-10 11:07:14 +0000

    Refactor SFA levy budget rule to use base payment rule methods

--- src/SFA.DAS.Payments.AcceptanceTests/Refactoring/Assertions/PaymentsAndEarningsRules/SfaLevyBudgetRule.cs
+++ src/SFA.DAS.Payments.AcceptanceTests/Refactoring/Assertions/PaymentsAndEarningsRules/SfaLevyBudgetRule.cs
@@ -1,42 +1,25 @@
-﻿using System;
-using System.Linq;
+﻿using System.Linq;
 using SFA.DAS.Payments.AcceptanceTests.Refactoring.Contexts;
 using SFA.DAS.Payments.AcceptanceTests.Refactoring.ReferenceDataModels;
-using SFA.DAS.Payments.AcceptanceTests.Refactoring.ResultsDataModels;
 
 namespace SFA.DAS.Payments.AcceptanceTests.Refactoring.Assertions.PaymentsAndEarningsRules
 {
-    public class SfaLevyBudgetRule : EarningsAndPaymentsRuleBase
+    public class SfaLevyBudgetRule : PaymentsRuleBase
     {
         public override void AssertBreakdown(EarningsAndPaymentsBreakdown breakdown, SubmissionContext submissionContext)
         {
-            var payments = GetEarningsForBreakdown(breakdown, submissionContext);
+            var payments = GetPaymentsForBreakdown(breakdown, submissionContext)
+                .Where(p => p.FundingSource == Enums.FundingSource.CoInvestedSfa && p.ContractType == Enums.ContractType.ContractWithEmployer)
+                .ToArray();
             foreach (var period in breakdown.SfaLevyBudget)
             {
                 AssertResultsForPeriod(period, payments);
             }
         }
 
-        private PaymentResult[] GetEarningsForBreakdown(EarningsAndPaymentsBreakdown breakdown, SubmissionContext submissionContext)
+        protected override string FormatAssertionFailureMessage(PeriodValue period, decimal actualPaymentInPeriod)
         {
-            var payments = submissionContext.SubmissionResults.Where(r => r.ProviderId.Equals(breakdown.ProviderId, StringComparison.CurrentCultureIgnoreCase));
-            if (breakdown is LearnerEarningsAndPaymentsBreakdown)
-            {
-                payments = payments.Where(r => r.LearnerId.Equals(((LearnerEarningsAndPaymentsBreakdown)breakdown).LearnerId, StringComparison.CurrentCultureIgnoreCase));
-            }
-            return payments.SelectMany(r => r.Payments).ToArray();
-        }
-        private void AssertResultsForPeriod(PeriodValue period, PaymentResult[] payments)
-        {
-            var budgetValue = payments.Where(r => r.CalculationPeriod == period.PeriodName && 
-                                            r.DeliveryPeriod == period.PeriodName
-                                            && r.FundingSource == Enums.FundingSource.CoInvestedSfa 
-                                            && r.ContractType == Enums.ContractType.ContractWithEmployer)
-                                            .Sum(r => r.Amount);
-            if (period.Value != budgetValue)
-            {
-                throw new Exception($"Expected sfa levy budget to be {period.Value} in {period.PeriodName} but actually was {budgetValue}");
-            }
+            return $"Expected SFA Levy budget to be debited {period.Value} in {period.PeriodName} but was actually debited {actualPaymentInPeriod}";
         }
     }
 }
