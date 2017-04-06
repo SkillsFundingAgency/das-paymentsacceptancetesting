a23abf0960449ce49e2573fcf2cbc28439cd9b6e
Simon Bull <simonbu11@users.noreply.github.com>
2017-04-06 14:57:46 +0100

    Export ILR files to meaningful place
    This should allow e2e testers to use files with little modification to test using PP DCFS

--- src/SFA.DAS.Payments.AcceptanceTests/SpecFlowHooks.cs
+++ src/SFA.DAS.Payments.AcceptanceTests/SpecFlowHooks.cs
@@ -1,4 +1,5 @@
-﻿using SFA.DAS.Payments.AcceptanceTests.ExecutionManagers;
+﻿using System.IO;
+using SFA.DAS.Payments.AcceptanceTests.ExecutionManagers;
 using TechTalk.SpecFlow;
 
 namespace SFA.DAS.Payments.AcceptanceTests
@@ -16,6 +17,28 @@ namespace SFA.DAS.Payments.AcceptanceTests
         public static void PrepareDatabasesForScenario()
         {
             PreperationManager.PrepareDatabasesForScenario();
+        }
+
+        [BeforeScenario]
+        public static void SetIlrFileDirectoryForScenario()
+        {
+            var scenarioDirectoryName = ScenarioContext.Current.ScenarioInfo.Title.Replace(",", "")
+                                                                                  .Replace("&", "and")
+                                                                                  .Replace("\\", "")
+                                                                                  .Replace(" ", "_");
+
+            TestEnvironment.Variables.IlrFileDirectory = Path.Combine(TestEnvironment.Variables.WorkingDirectory, "IlrFiles", scenarioDirectoryName);
+            if (Directory.Exists(TestEnvironment.Variables.IlrFileDirectory))
+            {
+                foreach (var file in Directory.GetFiles(TestEnvironment.Variables.IlrFileDirectory))
+                {
+                    File.Delete(file);
+                }
+            }
+            else
+            {
+                Directory.CreateDirectory(TestEnvironment.Variables.IlrFileDirectory);
+            }
         }
     }
 }
